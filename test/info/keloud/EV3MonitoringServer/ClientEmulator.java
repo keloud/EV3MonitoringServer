a95c7cf6bc42d02218777f80f76e7d217aeb01df
Keloud <toplaykisk@gmail.com>
2017-12-19 15:16:38 +0900

    修正のアップデート

--- test/info/keloud/EV3MonitoringServer/ClientEmulator.java
+++ test/info/keloud/EV3MonitoringServer/ClientEmulator.java
@@ -2,8 +2,8 @@ package info.keloud.EV3MonitoringServer;
 
 public class ClientEmulator {
     public static void main(String[] args) {
-        new ServerMain();
-        ServerMain.main(null);
+        //new ServerMain();
+        //ServerMain.main(null);
         new RandomSender();
     }
 }
